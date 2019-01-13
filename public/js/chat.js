abebceb10fe83427601bad5c9e966e3396877e59
ph <pawn13@gmail.com>
2019-01-13 18:45:15 +0300

    sadmin trip

--- public/js/chat.js
+++ public/js/chat.js
@@ -27,7 +27,7 @@ var highlighted_convos = ["General"];
 var start_press; // for long press detection
 var longpress = 400;
 
-var admins = ["Status","!!W1WIYPVSDg"]; // first trip here is used for server status posts
+var admins = ["Status","!!U6hZeQd.Tc"]; // first trip here is used for server status posts
 var devs = ["!!mtmxXFMsB2"];
 /* if you look at source you are essentially helping out, so have some blue colored trips! --> bluerules, testing */
 var default_contribs = ["!!Bk9pc/hnuA"];

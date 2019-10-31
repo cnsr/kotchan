02c95ac2d1b1bf0944fc569143c05e0456edf7fe
ph <pawn13@gmail.com>
2019-10-31 05:00:49 +0300

    stickers

--- stickers.py
+++ stickers.py
@@ -3,7 +3,7 @@ import os
 import sqlite3
 
 os.chdir('/home/ph/livechan-js')
-lb_db = sqlite3.connect('/home/ph/anna/lb.sqlite')
+lb_db = sqlite3.connect('/home/ph/anna3/lb.sqlite')
 count = dict(lb_db.execute('select name,count(*) from stickers group by name'))
 
 names = [os.path.splitext(f)[0] for f in os.listdir('public/images/stickers/') if f.endswith('png')]
